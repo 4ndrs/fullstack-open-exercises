f7f464aef7961a6282f9348d6fd16c2df55c4844
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-08 21:33:32 +0100

    Part1: (courseinfo) Refactor Content component

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -7,12 +7,19 @@ const App = () => {
   const part3 = "State of a component";
   const exercises3 = 14;
 
+  const partsAndExercises = {
+    part1,
+    part2,
+    part3,
+    exercises1,
+    exercises2,
+    exercises3,
+  };
+
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content {...partsAndExercises} />
       <Total list={[exercises1, exercises2, exercises3]} />
     </div>
   );
@@ -22,7 +29,17 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 
-const Content = ({ part, exercises }) => {
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+  );
+};
+
+const Part = ({ part, exercises }) => {
   return (
     <p>
       {part} {exercises}
