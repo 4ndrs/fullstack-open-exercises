d2145b4909ce1d9c0ff49d3b916e3e41181c55dd
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-09 12:13:14 +0100

    Part1: (courseinfo) Refactor with objects in array

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,26 +1,28 @@
 const App = () => {
   const course = "Half Stack application development";
 
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
 
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
 
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total list={[part1.exercises, part2.exercises, part3.exercises]} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 };
@@ -45,8 +47,8 @@ const Part = ({ name, exercises }) => {
   );
 };
 
-const Total = ({ list }) => {
-  const sumTotal = list.reduce((prev, curr) => prev + curr);
+const Total = ({ parts }) => {
+  const sumTotal = parts.reduce((prev, curr) => prev + curr.exercises, 0);
 
   return <p>Number of exercises {sumTotal}</p>;
 };
