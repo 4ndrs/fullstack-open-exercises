eb8081bb72bc508b4ceb4d204c290e61f23d3de7
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-11 00:50:43 +0100

    Part2: Add phonebook

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleAdd = (event) => {
+    event.preventDefault();
+    const newPersonObject = { name: newName };
+
+    setPersons(persons.concat(newPersonObject));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name:
+          <input
+            placeholder="Insert a new name"
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit" onClick={handleAdd}>
+            add
+          </button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
+    </div>
+  );
+};
+export default App;
