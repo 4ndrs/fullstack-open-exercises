2174e7f446b1cf03e93d906150ddc7177fae5ca5
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-10 14:44:04 +0100

    Part1: (unicafe) Add tbody to statistics' table

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -61,12 +61,14 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <table>
-        <StatisticLine text="good" value={good} />
-        <StatisticLine text="neutral" value={neutral} />
-        <StatisticLine text="bad" value={bad} />
-        <StatisticLine text="all" value={getTotal()} />
-        <StatisticLine text="average" value={getAverage()} />
-        <StatisticLine text="positive" value={getGoodPercent() + " %"} />
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={getTotal()} />
+          <StatisticLine text="average" value={getAverage()} />
+          <StatisticLine text="positive" value={getGoodPercent() + " %"} />
+        </tbody>
       </table>
     </div>
   );
