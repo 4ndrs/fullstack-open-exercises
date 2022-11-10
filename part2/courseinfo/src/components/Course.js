e44f46f74dc23ab7604bfdaf0dcc1283e8697c0e
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-10 23:05:49 +0100

    Part2: Add courseinfo

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,40 @@
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      <Total course={course} />
+    </div>
+  );
+};
+
+const Header = ({ course }) => {
+  return <h1>{course.name}</h1>;
+};
+
+const Content = ({ course }) => {
+  const parts = course.parts.map((part, index) => (
+    <Part key={index} name={part.name} exercises={part.exercises} />
+  ));
+
+  return <div>{parts}</div>;
+};
+
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Total = ({ course }) => {
+  const sumTotal = course.parts.reduce(
+    (prev, curr) => prev + curr.exercises,
+    0
+  );
+
+  return <p>Number of exercises {sumTotal}</p>;
+};
+
+export default Course;
