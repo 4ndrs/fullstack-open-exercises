f7e076b6a578e8e8cfc70a7e2a4937368ff4f5eb
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-11 01:40:08 +0100

    Part2: (phonebook) Add search field for filtering

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Arto Hellas", number: "040-1234567", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const handleAdd = (event) => {
     event.preventDefault();
@@ -22,14 +26,29 @@ const App = () => {
     setNewNumber("");
   };
 
+  const personsToShow = filter
+    ? persons.filter((person) =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      )
+    : persons;
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        filter shown with
+        <input
+          value={filter}
+          onChange={(event) => setFilter(event.target.value)}
+        />
+      </div>
+
+      <h2>add a new</h2>
       <form>
         <div>
           name:
           <input
-            placeholder="Insert a new name"
             value={newName}
             onChange={(event) => setNewName(event.target.value)}
           />
@@ -37,7 +56,6 @@ const App = () => {
         <div>
           number:
           <input
-            placeholder="Insert a new number"
             value={newNumber}
             onChange={(event) => setNewNumber(event.target.value)}
           />
@@ -50,7 +68,7 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {personsToShow.map((person) => (
         <div key={person.name}>
           {person.name} {person.number}
         </div>
