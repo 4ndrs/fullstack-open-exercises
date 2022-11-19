4f9f0b25f84a7e9094457a9086b5fbfb4833b321
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-19 12:35:40 +0100

    Part4: (bloglist) Move Blog model to models/blog.js

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -3,17 +3,9 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 require("dotenv").config({ path: "./.env.local" });
+const Blog = require("./models/blog.js");
 
 const app = express();
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
 
 const mongoUrl = process.env.MONGODB_URI;
 mongoose.connect(mongoUrl);
