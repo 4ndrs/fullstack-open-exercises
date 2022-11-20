12cbb04257743bcb0d989f3ac2206122c322f081
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-20 19:02:15 +0100

    Part4: (bloglist) Add favoriteBlog helper function and tests

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -64,3 +64,65 @@ describe("total likes", () => {
     expect(result).toBe(18);
   });
 });
+
+describe("favorite blog", () => {
+  test("when list has one blog, favorite equals that", () => {
+    const blogs = [
+      {
+        _id: "6378bf2184c3726a3b25140e",
+        title: "The Daily Life of a Black Cat",
+        author: "Fran",
+        url: "https://blog.tenken-anime.com/",
+        likes: 4,
+        __v: 0,
+      },
+    ];
+
+    const result = listHelper.favoriteBlog(blogs);
+    const blog = {
+      title: "The Daily Life of a Black Cat",
+      author: "Fran",
+      likes: 4,
+    };
+
+    expect(result).toEqual(blog);
+  });
+
+  test("when list has many, favorite equals the one with most likes", () => {
+    const blogs = [
+      {
+        _id: "6378aa33530fa63025069e23",
+        title: "Management of a Novice Alchemist",
+        author: "Sarasa Feed",
+        url: "https://blog.shinmai-renkin.com/",
+        likes: 4,
+        __v: 0,
+      },
+      {
+        _id: "6378bf2184c3726a3b25140e",
+        title: "The Daily Life of a Black Cat",
+        author: "Fran",
+        url: "https://blog.tenken-anime.com/",
+        likes: 4,
+        __v: 0,
+      },
+      {
+        _id: "6378d4a25e36d28166d943f3",
+        title: "Achtung – Panzer!",
+        author: "Yukari Akiyama",
+        url: "https://blog.girls-und-panzer.jp/",
+        likes: 10,
+        __v: 0,
+      },
+    ];
+
+    const result = listHelper.favoriteBlog(blogs);
+    const blog = {
+      title: "Achtung – Panzer!",
+      author: "Yukari Akiyama",
+      likes: 10,
+    };
+
+    expect(result).toEqual(blog);
+  });
+});
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,7 +6,27 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length < 1) {
+    return;
+  }
+
+  const blog = blogs.reduce((favorite, current) => {
+    if (current.likes < favorite.likes) {
+      return favorite;
+    }
+    return current;
+  });
+
+  return {
+    title: blog.title,
+    author: blog.author,
+    likes: blog.likes,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
