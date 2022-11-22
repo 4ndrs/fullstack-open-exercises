7e165eb9021f3c16d6550cc42405569763d08032
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-22 15:39:01 +0100

    Part4: (bloglist) Refactor blog_api.test.js

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -9,80 +9,80 @@ const api = supertest(app);
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-
-  const blogs = helper.initialBlogs.map((blog) => new Blog(blog));
-  const promiseArray = blogs.map((blog) => blog.save());
-
-  await Promise.all(promiseArray);
+  await Blog.insertMany(helper.initialBlogs);
 });
 
-test("all blogs are returned in the JSON format", async () => {
-  const response = await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("when some blog posts exist", () => {
+  test("all blogs are returned in the JSON format", async () => {
+    const response = await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
 
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
+
+  test("id property of a specific blog is defined", async () => {
+    const response = await api.get("/api/blogs");
+    const note = response.body[0];
+
+    expect(note.id).toBeDefined();
+  });
 });
 
-test("id property of a note is defined", async () => {
-  const response = await api.get("/api/blogs");
-  const note = response.body[0];
+describe("create blog post", () => {
+  test("verify the post created is in the database", async () => {
+    const newBlog = {
+      title: "Alchemy 101: The Importance of Networking",
+      author: "Sarasa Feed",
+      url: "https://blog.shinmai-renkin.com/",
+      likes: 0,
+    };
 
-  expect(note.id).toBeDefined();
-});
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-test("create a new blog post", async () => {
-  const newBlog = {
-    title: "Alchemy 101: The Importance of Networking",
-    author: "Sarasa Feed",
-    url: "https://blog.shinmai-renkin.com/",
-    likes: 0,
-  };
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    const contents = blogsAtEnd.map((blog) => blog.title);
+    expect(contents).toContain(newBlog.title);
+  });
 
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+  test("if likes is missing, default to zero", async () => {
+    const newBlog = {
+      title: "Glow at the Velocity of Light",
+      author: "Aries Spring",
+      url: "https://blog.astra-anime.com/",
+    };
 
-  const contents = blogsAtEnd.map((blog) => blog.title);
-  expect(contents).toContain(newBlog.title);
-});
+    const response = await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-test("if likes is missing, default to zero", async () => {
-  const newBlog = {
-    title: "Glow at the Velocity of Light",
-    author: "Aries Spring",
-    url: "https://blog.astra-anime.com/",
-  };
+    expect(response.body.likes).toBe(0);
+  });
 
-  const response = await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+  test("if title or url is missing, return 400", async () => {
+    const newBlog = {
+      title: "The Spiral of Happiness",
+      author: "Kikuri Hiroi",
+    };
 
-  expect(response.body.likes).toBe(0);
-});
+    await api.post("/api/blogs").send(newBlog).expect(400);
 
-test("if title or url is missing, return 400", async () => {
-  const newBlog = {
-    title: "The Spiral of Happiness",
-    author: "Kikuri Hiroi",
-  };
+    const newBlog2 = {
+      author: "Kikuri Hiroi",
+      url: "https://bocchi.rocks/blog/",
+    };
 
-  await api.post("/api/blogs").send(newBlog).expect(400);
-
-  const newBlog2 = {
-    author: "Kikuri Hiroi",
-    url: "https://bocchi.rocks/blog/",
-  };
-
-  await api.post("/api/blogs").send(newBlog2).expect(400);
+    await api.post("/api/blogs").send(newBlog2).expect(400);
+  });
 });
 
 afterAll(() => {
