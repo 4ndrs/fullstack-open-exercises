17fdb3eb1f9b79fe44f846bdca8fcbb2522e0f18
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-22 15:58:06 +0100

    Part4: (bloglist) Add test for deleting a blog post

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -85,6 +85,22 @@ describe("create blog post", () => {
   });
 });
 
+describe("delete blog post", () => {
+  test("deleting an existing id returns 204", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
+
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+    const blogsAtEnd = await helper.blogsInDb();
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+    const contents = blogsAtEnd.map((blog) => blog.title);
+
+    expect(contents).not.toContain(blogToDelete.title);
+  });
+});
+
 afterAll(() => {
   mongoose.connection.close();
 });
