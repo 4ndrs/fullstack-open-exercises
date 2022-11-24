fbf5caa427b933bc2155d9aa8d4d2c27fb7b89dd
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-24 15:11:34 +0100

    Part4: (bloglist) Add user router

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const blogsRouter = require("./controllers/blogs.js");
+const usersRouter = require("./controllers/users.js");
 const { MONGODB_URI } = require("./utils/config.js");
 
 const app = express();
@@ -13,5 +14,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
