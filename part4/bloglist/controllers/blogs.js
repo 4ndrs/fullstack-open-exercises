6a48e35eaa8af938b41febaa8e91e93e7b984279
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-24 23:15:49 +0100

    Part4: (bloglist) Populate users on get requests in blogsRouter

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,7 +3,11 @@ const blogsRouter = require("express").Router();
 const Blog = require("../models/blog.js");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+    id: 1,
+  });
   response.json(blogs);
 });
 
