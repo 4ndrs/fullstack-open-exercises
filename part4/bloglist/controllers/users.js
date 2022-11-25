2d052249d5ce30d68fac8e205343e3972a8cf878
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-25 12:30:02 +0100

    Part4: (bloglist) Populate blogs in usersRouter get requests

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -4,7 +4,12 @@ const bcrypt = require("bcrypt");
 const User = require("../models/user.js");
 
 usersRouter.get("/", async (request, response) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("blogs", {
+    url: 1,
+    title: 1,
+    author: 1,
+    id: 1,
+  });
   response.json(users);
 });
 
