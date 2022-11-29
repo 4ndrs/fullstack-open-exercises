fa285467995933e8d00e90329929984cec7a7f50
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-29 03:03:30 +0100

    Part5: (bloglist-frontend) Return LoginForm first if not logged in

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -20,17 +20,17 @@ const App = () => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
   }, []);
 
-  if (user) {
-    return (
-      <>
-        <h2>blogs</h2>
-        <LoggedUser setUser={setUser} userName={user.name} />
-        <Blogs blogs={blogs} />
-      </>
-    );
+  if (!user) {
+    return <LoginForm setUser={setUser} />;
   }
 
-  return <LoginForm setUser={setUser} />;
+  return (
+    <>
+      <h2>blogs</h2>
+      <LoggedUser setUser={setUser} userName={user.name} />
+      <Blogs blogs={blogs} />
+    </>
+  );
 };
 
 export default App;
