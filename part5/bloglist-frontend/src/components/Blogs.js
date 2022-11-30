d95a10ca471d32b7c32942fa269d53ec46d204d7
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-30 07:15:43 +0100

    Part5: (bloglist-frontend) Sort blog posts by likes

--- part5/bloglist-frontend/src/components/Blogs.js
+++ part5/bloglist-frontend/src/components/Blogs.js
@@ -3,9 +3,11 @@ import Blog from "./Blog";
 const Blogs = ({ blogs, setBlogs }) => {
   return (
     <>
-      {blogs.map((blog) => (
-        <Blog key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs} />
-      ))}
+      {blogs
+        .sort((a, b) => b.likes - a.likes)
+        .map((blog) => (
+          <Blog key={blog.id} blog={blog} blogs={blogs} setBlogs={setBlogs} />
+        ))}
     </>
   );
 };
