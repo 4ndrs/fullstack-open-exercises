4405309476af364e00f9b96abecba2b9573c1a1c
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-30 04:14:49 +0100

    Part5: (bloglist-frontend) Add view/hide button to Blog component

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,31 @@
+import { useState } from "react";
+
+import "./Blog.css";
+
 const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
   return (
-    <div>
-      {blog.title} {blog.author}
+    <div className="blog">
+      {blog.title} {blog.author}{" "}
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? "hide" : "view"}
+      </button>
+      {showDetails && <Details blog={blog} />}
     </div>
+  );
+};
+
+const Details = ({ blog }) => {
+  return (
+    <>
+      <div>{blog.url}</div>
+      <div>
+        likes {blog.likes}
+        <button>like</button>
+      </div>
+      <div>{blog.user?.name}</div>
+    </>
   );
 };
 
