8f27733312746d9f917476f88daaaf4a56262cc5
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-01 22:51:25 +0100

    Part5: (bloglist-frontend) Add test for checking the like button event
    handler

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -44,3 +44,35 @@ test("displays the url and number of likes when the view button is clicked", asy
   screen.getByText(blog.url);
   screen.getByText(`likes ${blog.likes}`);
 });
+
+test("if the like button is clicked twice, the event handler is called twice", async () => {
+  const user = userEvent.setup();
+  const handleLike = jest.fn();
+  const blog = {
+    title: "KITAAAAAAAAAAAAAN",
+    author: "Ikuyo Kita",
+    url: "https://bocchi.rocks/blog",
+    likes: 56,
+    user: {
+      username: "yurika",
+      name: "Yurika Toudou",
+    },
+  };
+
+  render(
+    <Blog
+      blog={blog}
+      user={{ username: "wataru" }}
+      handleUpdateBlog={handleLike}
+    />
+  );
+
+  await user.click(screen.getByRole("button", { name: "view" }));
+
+  const likeButton = screen.getByRole("button", { name: "like" });
+
+  await user.click(likeButton);
+  await user.click(likeButton);
+
+  expect(handleLike).toHaveBeenCalledTimes(2);
+});
