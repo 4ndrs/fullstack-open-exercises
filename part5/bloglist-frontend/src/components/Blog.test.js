d0d78b32e62a08a56005e6c7bdff493bf884134e
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-01 22:31:50 +0100

    Part5: (bloglist-frontend) Add test for checking if the url and likes
    are displayed after the view button is clicked

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,5 +1,6 @@
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import Blog from "./Blog";
 
@@ -21,4 +22,25 @@ test("displays title & author, but not url or number of likes by default", () =>
 
   expect(screen.queryByText(blog.url)).not.toBeInTheDocument();
   expect(screen.queryByText(`likes ${blog.likes}`)).not.toBeInTheDocument();
+});
+
+test("displays the url and number of likes when the view button is clicked", async () => {
+  const user = userEvent.setup();
+  const blog = {
+    title: "KITAAAAAAAAAAAAAN",
+    author: "Ikuyo Kita",
+    url: "https://bocchi.rocks/blog",
+    likes: 56,
+    user: {
+      username: "yurika",
+      name: "Yurika Toudou",
+    },
+  };
+
+  render(<Blog blog={blog} user={{ username: "wataru" }} />);
+
+  await user.click(screen.getByRole("button", { name: "view" }));
+
+  screen.getByText(blog.url);
+  screen.getByText(`likes ${blog.likes}`);
 });
