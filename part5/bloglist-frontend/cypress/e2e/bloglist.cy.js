cc9fa4b69bcedcf4404e09c979a11555b670164d
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-03 05:54:36 +0100

    Part5: (bloglist-frontend) Add tests to check logging in

--- part5/bloglist-frontend/cypress/e2e/bloglist.cy.js
+++ part5/bloglist-frontend/cypress/e2e/bloglist.cy.js
@@ -1,6 +1,13 @@
 describe("Blog app", () => {
   beforeEach(() => {
+    const newUser = {
+      name: "Kamen Rider Kaixa",
+      username: "kaixa",
+      password: "913",
+    };
+
     cy.request("POST", "http://localhost:3003/api/testing/reset");
+    cy.createUser(newUser);
     cy.visit("http://localhost:3000");
   });
 
@@ -8,5 +15,27 @@ describe("Blog app", () => {
     cy.contains("username").find("input");
     cy.contains("password").find("input");
     cy.contains("login");
+  });
+
+  describe("Login", () => {
+    it("succeeds with correct credentials", () => {
+      cy.contains("username").find("input").type("kaixa");
+      cy.contains("password").find("input").type("913");
+      cy.contains("login").click();
+
+      cy.contains("Kamen Rider Kaixa logged in")
+        .find("button")
+        .contains("logout");
+    });
+
+    it("fails with wrong credentials", () => {
+      cy.contains("username").find("input").type("kaixa");
+      cy.contains("password").find("input").type("555");
+      cy.contains("login").click();
+
+      cy.contains("invalid username or password")
+        .should("have.class", "error")
+        .and("have.css", "color", "rgb(255, 0, 0)");
+    });
   });
 });
