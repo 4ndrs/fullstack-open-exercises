3bc96e143360aecaf351684f29482730f1c4931e
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-04 20:50:44 +0100

    Part6: (redux-anecdotes) Move store creation to store.js
    And use Redux Toolkit for its configuration

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -5,7 +5,7 @@ import { vote } from "../reducers/anecdoteReducer";
 const AnecdoteList = () => {
   const dispatch = useDispatch();
   const anecdotes = useSelector((state) =>
-    [...state].sort((a, b) => b.votes - a.votes)
+    [...state.anecdotes].sort((a, b) => b.votes - a.votes)
   );
 
   return (
