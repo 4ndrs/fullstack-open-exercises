f97b26b156ef27bb8f8bf2e997d48e241e130bf4
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-05 02:49:56 +0100

    Part6: (redux-anecdotes) Add notifications to vote

--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,12 +1,18 @@
 import { useSelector, useDispatch } from "react-redux";
 
 import { vote } from "../reducers/anecdoteReducer";
+import { setNotification } from "../reducers/notificationReducer";
 
 const AnecdoteList = () => {
   const dispatch = useDispatch();
   const anecdotes = useSelector((state) =>
     [...state.anecdotes].sort((a, b) => b.votes - a.votes)
   );
+
+  const handleVote = (anecdote) => {
+    dispatch(vote(anecdote.id));
+    dispatch(setNotification(`you voted '${anecdote.content}'`));
+  };
 
   return (
     <>
@@ -15,7 +21,7 @@ const AnecdoteList = () => {
           <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
-            <button onClick={() => dispatch(vote(anecdote.id))}>vote</button>
+            <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
       ))}
--- part6/redux-anecdotes/src/components/Notification.js
+++ part6/redux-anecdotes/src/components/Notification.js
@@ -1,13 +1,28 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+
+import { resetNotification } from "../reducers/notificationReducer";
 
 const Notification = () => {
+  const dispatch = useDispatch();
   const notification = useSelector((state) => state.notification);
+
+  useEffect(() => {
+    if (notification) {
+      const id = setTimeout(() => dispatch(resetNotification()), 5000);
+      return () => clearTimeout(id);
+    }
+  }, [notification, dispatch]);
 
   const style = {
     border: "solid",
     padding: 10,
     borderWidth: 1,
   };
+
+  if (!notification) {
+    return;
+  }
 
   return <div style={style}>{notification}</div>;
 };
