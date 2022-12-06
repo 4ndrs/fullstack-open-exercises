aed112abbe773b2ec6226cef5d4aa6a7d9787a1f
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-06 09:01:00 +0100

    Part7: (routed-anecdotes) Implement view for a single anecdote

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useMatch } from "react-router-dom";
 
 const Menu = () => {
   const padding = {
@@ -25,10 +25,22 @@ const AnecdoteList = ({ anecdotes }) => (
     <h2>Anecdotes</h2>
     <ul>
       {anecdotes.map((anecdote) => (
-        <li key={anecdote.id}>{anecdote.content}</li>
+        <li key={anecdote.id}>
+          <Link to={`/anecdotes/${anecdote.id}`}>{anecdote.content}</Link>
+        </li>
       ))}
     </ul>
   </div>
+);
+
+const Anecdote = ({ anecdote }) => (
+  <>
+    <h1>
+      {anecdote.content} by {anecdote.author}
+    </h1>
+    <p>has {anecdote.votes} votes</p>
+    <p>for more info see {anecdote.info}</p>
+  </>
 );
 
 const About = () => (
@@ -132,6 +144,11 @@ const App = () => {
   ]);
 
   const [notification, setNotification] = useState("");
+  const match = useMatch("/anecdotes/:id");
+
+  const anecdote = match
+    ? anecdotes.find((anecdote) => anecdote.id === Number(match.params.id))
+    : null;
 
   const addNew = (anecdote) => {
     anecdote.id = Math.round(Math.random() * 10000);
@@ -152,18 +169,22 @@ const App = () => {
   };
 
   return (
-    <Router>
+    <>
       <h1>Software anecdotes</h1>
       <Menu />
 
       <Routes>
         <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />} />
+        <Route
+          path="/anecdotes/:id"
+          element={<Anecdote anecdote={anecdote} />}
+        />
         <Route path="/about" element={<About />} />
         <Route path="/create" element={<CreateNew addNew={addNew} />} />
       </Routes>
 
       <Footer />
-    </Router>
+    </>
   );
 };
 
