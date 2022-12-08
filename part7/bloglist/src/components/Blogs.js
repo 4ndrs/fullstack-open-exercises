1f819a06299d05769da0c63fdad2739467914ec6
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-08 14:49:08 +0100

    Part7: (bloglist) Merge Blog into Blogs

--- part7/bloglist/src/components/Blogs.js
+++ part7/bloglist/src/components/Blogs.js
@@ -1,9 +1,10 @@
-import { useRef } from "react";
-import { useSelector } from "react-redux";
+import { useRef, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import Blog from "./Blog";
+import "./Blog.css";
 import Toggler from "./Toggler";
 import CreateForm from "./CreateForm";
+import { updateBlog, removeBlog } from "../reducers/blogsReducer";
 
 const Blogs = () => {
   const blogs = useSelector((state) => state.blogs);
@@ -14,6 +15,70 @@ const Blogs = () => {
         .map((blog) => (
           <Blog key={blog.id} blog={blog} />
         ))}
+    </>
+  );
+};
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  return (
+    <div className="blog">
+      {blog.title} {blog.author}{" "}
+      <button onClick={() => setShowDetails(!showDetails)}>
+        {showDetails ? "hide" : "view"}
+      </button>
+      {showDetails && <Details blog={blog} />}
+    </div>
+  );
+};
+
+const Details = ({ blog }) => {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => {
+    return {
+      username: state.loggedUser.username,
+    };
+  });
+
+  const handleLike = async () => {
+    const likedBlog = {
+      ...blog,
+      likes: blog.likes + 1,
+    };
+
+    try {
+      await dispatch(updateBlog(likedBlog));
+    } catch (exception) {
+      console.log(exception);
+    }
+  };
+
+  const handleRemove = async () => {
+    const msg = `Remove blog ${blog.title} by ${blog.author}`;
+
+    if (window.confirm(msg)) {
+      try {
+        await dispatch(removeBlog(blog.id));
+      } catch (exception) {
+        console.log(exception);
+      }
+    }
+  };
+
+  return (
+    <>
+      <div>{blog.url}</div>
+      <div>
+        likes {blog.likes}
+        <button onClick={handleLike}>like</button>
+      </div>
+      <div>{blog.user?.name}</div>
+      <div>
+        {user.username === blog.user?.username && (
+          <button onClick={handleRemove}>remove</button>
+        )}
+      </div>
     </>
   );
 };
