5f6bd2dc42ec4b1ca3cade85eeefcf82bb7b0f2d
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-08 20:37:25 +0100

    Part7: (bloglist) Move the frontend to frontend folder

--- part7/bloglist/frontend/src/App.js
+++ part7/bloglist/frontend/src/App.js
@@ -2,15 +2,16 @@ import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
+import { initializeUsers } from "./reducers/usersReducer";
+import { initializeBlogs } from "./reducers/blogsReducer";
+import { initializeLoggedUser } from "./reducers/loggedUserReducer";
+
 import User from "./components/User";
 import Blog from "./components/Blog";
 import Blogs from "./components/Blogs";
 import Users from "./components/Users";
 import Header from "./components/Header";
 import LoginForm from "./components/LoginForm";
-import { initializeLoggedUser } from "./reducers/loggedUserReducer";
-import { initializeBlogs } from "./reducers/blogsReducer";
-import { initializeUsers } from "./reducers/usersReducer";
 
 const App = () => {
   const dispatch = useDispatch();
