3584516dac2b7deec20b4a06f6b726af0c363f5e
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-08 22:50:29 +0100

    Part7: (bloglist-backend) Make comments a subSchema and transform ids

--- part7/bloglist/backend/models/blog.js
+++ part7/bloglist/backend/models/blog.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
+const commentSchema = new mongoose.Schema({
+  author: String,
+  date: Date,
+  message: String,
+});
+
 const blogSchema = new mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number,
-  comments: [
-    {
-      author: String,
-      date: Date,
-      message: String,
-    },
-  ],
+  comments: [commentSchema],
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User",
@@ -23,6 +23,13 @@ blogSchema.set("toJSON", {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
+  },
+});
+
+commentSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
   },
 });
 
