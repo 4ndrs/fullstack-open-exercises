3b5ca911e9017b6a3cfca056454c6af39530d478
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-09 23:48:00 +0100

    Part7: (bloglist-frontend) Add styles to LoggedUser component

--- part7/bloglist/frontend/src/components/LoggedUser.js
+++ part7/bloglist/frontend/src/components/LoggedUser.js
@@ -1,4 +1,7 @@
+import LogoutIcon from "@mui/icons-material/Logout";
 import { useSelector, useDispatch } from "react-redux";
+import { Typography, Box, IconButton, Tooltip } from "@mui/material";
+
 import { resetLoggedUser } from "../reducers/loggedUserReducer";
 
 const LoggedUser = () => {
@@ -6,10 +9,25 @@ const LoggedUser = () => {
   const name = useSelector((state) => state.loggedUser.name);
 
   return (
-    <>
-      {name} logged in{" "}
-      <button onClick={() => dispatch(resetLoggedUser())}>logout</button>
-    </>
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        gap: "10px",
+      }}
+    >
+      <Typography>{name} logged in</Typography>
+      <Tooltip title="Logout">
+        <IconButton
+          aria-label="logout"
+          size="small"
+          onClick={() => dispatch(resetLoggedUser())}
+        >
+          <LogoutIcon />
+        </IconButton>
+      </Tooltip>
+    </Box>
   );
 };
 
