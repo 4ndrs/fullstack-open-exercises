d69945ee0a112bbed7c3db099b7c4de8687974be
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-09 17:24:33 +0100

    Part7: (bloglist-frontend) Refactor notification for slide transition

--- part7/bloglist/frontend/src/components/Notification.js
+++ part7/bloglist/frontend/src/components/Notification.js
@@ -1,20 +1,37 @@
+import { useRef } from "react";
 import { useSelector } from "react-redux";
-import { Snackbar, Alert } from "@mui/material";
+import { Snackbar, Alert, Slide } from "@mui/material";
 
 const Notification = () => {
+  const id = useRef(null);
+  const error = useRef(false);
   const notification = useSelector((state) => state.notification);
 
-  const severity = notification.error ? "error" : "success";
-  const open = Boolean(notification.text);
+  if (notification.id !== null) {
+    id.current = notification.id;
+  }
+
+  if (notification.error !== null) {
+    error.current = notification.error;
+  }
+
+  const severity = error.current ? "error" : "success";
+  const open = notification.id !== null ? Boolean(notification.text) : false;
 
   return (
     <Snackbar
       open={open}
+      TransitionComponent={SlideTransition}
+      key={id.current}
       anchorOrigin={{ vertical: "top", horizontal: "right" }}
     >
       <Alert severity={severity}>{notification.text}</Alert>
     </Snackbar>
   );
+};
+
+const SlideTransition = (props) => {
+  return <Slide {...props} direction="left" />;
 };
 
 export default Notification;
