ea1bb42d2de884d08f3e67fae8d26b8b8b602875
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-10 02:26:08 +0100

    Part7: (bloglist-frontend) Add styles to CreateForm component

--- part7/bloglist/frontend/src/components/CreateForm.js
+++ part7/bloglist/frontend/src/components/CreateForm.js
@@ -1,22 +1,30 @@
 import { useState } from "react";
 import { useDispatch } from "react-redux";
+import { LoadingButton } from "@mui/lab";
+import { Box, Typography, TextField } from "@mui/material";
 
 import { addBlog } from "../reducers/blogsReducer";
 import { setNotification } from "../reducers/notificationReducer";
 
 const CreateForm = ({ togglerRef }) => {
-  const [title, setTitle] = useState("");
-  const [author, setAuthor] = useState("");
-  const [url, setUrl] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const dispatch = useDispatch();
 
   const handleCreate = async (event) => {
     event.preventDefault();
-    const newBlog = { title, author, url };
+    const data = new FormData(event.currentTarget);
+
+    setLoading(true);
+    const newBlog = {
+      title: data.get("title"),
+      author: data.get("author"),
+      url: data.get("url"),
+    };
 
     try {
       await dispatch(addBlog(newBlog));
+      setLoading(false);
       const text = `${newBlog.title} by ${newBlog.author} added`;
 
       dispatch(setNotification(text));
@@ -27,30 +35,32 @@ const CreateForm = ({ togglerRef }) => {
   };
 
   return (
-    <>
-      <h2>create new</h2>
-      <form onSubmit={handleCreate}>
-        <div>
-          title:
-          <input
-            value={title}
-            onChange={({ target }) => setTitle(target.value)}
-          />
-        </div>
-        <div>
-          author:
-          <input
-            value={author}
-            onChange={({ target }) => setAuthor(target.value)}
-          />
-        </div>
-        <div>
-          url:
-          <input value={url} onChange={({ target }) => setUrl(target.value)} />
-        </div>
-        <button type="submit">create</button>
-      </form>
-    </>
+    <Box>
+      <Typography align="center" variant="h4" sx={{ m: 2 }}>
+        Create New
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleCreate}
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <TextField required margin="normal" label="Title" name="title" />
+        <TextField required margin="normal" label="Author" name="author" />
+        <TextField required margin="normal" label="Url" name="url" type="url" />
+        <LoadingButton
+          loading={loading}
+          variant="contained"
+          type="submit"
+          sx={{ mb: 1 }}
+        >
+          create
+        </LoadingButton>
+      </Box>
+    </Box>
   );
 };
 
