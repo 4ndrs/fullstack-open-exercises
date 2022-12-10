08b4eb7de65a62b72a9a05bdb01086f34e65649b
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-10 03:18:47 +0100

    Part7: (bloglist-frontend) Add styles to Users component

--- part7/bloglist/frontend/src/components/Users.js
+++ part7/bloglist/frontend/src/components/Users.js
@@ -1,32 +1,49 @@
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
+import {
+  Typography,
+  Table,
+  TableHead,
+  TableRow,
+  TableCell,
+  TableBody,
+  Button,
+} from "@mui/material";
+
 const Users = () => {
   const users = useSelector((state) => state.users);
 
   return (
     <>
-      <h2>Users</h2>
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>
-              <strong>blogs created</strong>
-            </th>
-          </tr>
-        </thead>
-        <tbody>
+      <Typography variant="h3" sx={{ mb: 5, mt: 5 }}>
+        Users
+      </Typography>
+
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell></TableCell>
+            <TableCell>
+              <Typography>
+                <strong>Blogs created</strong>
+              </Typography>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
           {users.map((user) => (
-            <tr key={user.id}>
-              <td>
-                <Link to={`/users/${user.id}`}>{user.name}</Link>
-              </td>
-              <td>{user.blogs.length}</td>
-            </tr>
+            <TableRow key={user.id}>
+              <TableCell>
+                <Button component={Link} to={`/users/${user.id}`}>
+                  {user.name}
+                </Button>
+              </TableCell>
+              <TableCell>{user.blogs.length}</TableCell>
+            </TableRow>
           ))}
-        </tbody>
-      </table>
+        </TableBody>
+      </Table>
     </>
   );
 };
