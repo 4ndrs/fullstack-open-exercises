035a9454d7a859577ebbbd0e8d9c0333addc9c8f
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-13 08:11:44 +0100

    Part8: (library-frontend) Refetch allBooks by name after mutation
    This triggers the refetching of both the query with the variables and
    without, fixing the book view when filtering by genre is selected. With
    this fix, the view is updated right away, even when the genre is selected.

--- part8/library-frontend/src/components/NewBook.js
+++ part8/library-frontend/src/components/NewBook.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useMutation } from "@apollo/client";
 
-import { ADD_BOOK, ALL_BOOKS, ALL_AUTHORS } from "../queries.js";
+import { ADD_BOOK, ALL_AUTHORS } from "../queries.js";
 
 const NewBook = (props) => {
   const [title, setTitle] = useState("");
@@ -11,7 +11,7 @@ const NewBook = (props) => {
   const [genres, setGenres] = useState([]);
 
   const [addBook] = useMutation(ADD_BOOK, {
-    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }],
+    refetchQueries: ["allBooks", { query: ALL_AUTHORS }],
   });
 
   if (!props.show) {
