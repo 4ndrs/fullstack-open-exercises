86b45f7d7a4eb4e66b549bed4d0912e8cdee8dfc
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-13 03:31:34 +0100

    Part8: (library-frontend) Add authentication

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,24 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useApolloClient } from "@apollo/client";
+
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import LoginForm from "./components/LoginForm";
 
 const App = () => {
   const [page, setPage] = useState("authors");
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("loggedLibraryAppUser");
+    setToken(token);
+  }, []);
+
+  const client = useApolloClient();
+
+  const handleLogin = (token) => {
+    setToken(token);
+  };
+
+  const handleLogout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
 
   return (
     <div>
       <div>
         <button onClick={() => setPage("authors")}>authors</button>
         <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
+        {token ? (
+          <button onClick={() => setPage("add")}>add book</button>
+        ) : (
+          <button onClick={() => setPage("login")}>login</button>
+        )}
+        {token && <button onClick={handleLogout}>logout</button>}
       </div>
 
       <Authors show={page === "authors"} />
-
       <Books show={page === "books"} />
-
       <NewBook show={page === "add"} />
+      <LoginForm show={page === "login"} handleLogin={handleLogin} />
     </div>
   );
 };
