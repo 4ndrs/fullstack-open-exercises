4852aa84d6bd203f70dfacce519febdb6b7a3ac6
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-16 19:01:18 +0100

    Part9: (patientor-backend) Add cors and change port
    Needed to work with the frontend

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,13 +1,16 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.get("/api/ping", (_request, response) => {
   console.log("pin pon pin pon!");
   response.send("pong");
 });
 
-const PORT = 3003;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
