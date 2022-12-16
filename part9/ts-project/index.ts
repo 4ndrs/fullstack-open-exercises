dfdadb956efdbdc20974630e685c0447436925f0
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-16 01:25:47 +0100

    Part9: (ts-project) Add bmi endpoint

--- part9/ts-project/index.ts
+++ part9/ts-project/index.ts
@@ -1,9 +1,24 @@
 import express from "express";
 
+import { calculateBmi } from "./bmiCalculator";
+
 const app = express();
 
 app.get("/hello", (_request, response) => {
   response.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (request, response) => {
+  const height = Number(request.query.height);
+  const weight = Number(request.query.weight);
+
+  if ([height, weight].includes(NaN)) {
+    return response.status(400).send({ error: "malformatted parameters" });
+  }
+
+  const bmi = calculateBmi(height, weight);
+
+  return response.send({ weight, height, bmi });
 });
 
 const PORT = 3003;
