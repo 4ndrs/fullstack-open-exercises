c114b569f5c616f699c44aedddabb9ac089e3c6b
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-16 05:31:38 +0100

    Part9: (ts-project) Add exercises endpoint

--- part9/ts-project/index.ts
+++ part9/ts-project/index.ts
@@ -1,8 +1,10 @@
 import express from "express";
 
 import { calculateBmi } from "./bmiCalculator";
+import { calculateExercises } from "./exerciseCalculator";
 
 const app = express();
+app.use(express.json());
 
 app.get("/hello", (_request, response) => {
   response.send("Hello Full Stack!");
@@ -27,6 +29,50 @@ app.get("/bmi", (request, response) => {
     }
 
     return response.status(400).send({ error: errorMessage });
+  }
+});
+
+interface ExercisesRequestBody {
+  daily_exercises: unknown;
+  target: unknown;
+}
+
+interface ExercisesRequest {
+  body: ExercisesRequestBody;
+}
+
+app.post("/exercises", (request: ExercisesRequest, response) => {
+  const missingParamsMessage = { error: "parameters missing" };
+  const malformattedParamsMessage = { error: "malformatted parameters" };
+
+  if ([request.body.daily_exercises, request.body.target].includes(undefined)) {
+    return response.status(400).send(missingParamsMessage);
+  }
+
+  if (!(request.body.daily_exercises instanceof Array)) {
+    return response.status(400).send(malformattedParamsMessage);
+  }
+
+  const target = Number(request.body.target);
+  const dailyExerciseHours = request.body.daily_exercises.map((hour) =>
+    Number(hour)
+  );
+
+  if (
+    [...dailyExerciseHours, target].includes(NaN) ||
+    [...dailyExerciseHours, target].includes(Infinity)
+  ) {
+    return response.status(400).send(malformattedParamsMessage);
+  }
+
+  try {
+    const result = calculateExercises(dailyExerciseHours, target);
+    return response.send(result);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.log(error);
+    }
+    return response.status(400).send(malformattedParamsMessage);
   }
 });
 
