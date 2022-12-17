0483eab4e17447f6b72411bce1f1ea2034f32a53
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-17 13:24:11 +0100

    Part9: (patientor-backend) Add patients route

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import diagnoseRouter from "./routes/diagnoses";
+import patientRouter from "./routes/patients";
 
 const app = express();
 
 app.use(cors());
 app.use("/api/diagnoses", diagnoseRouter);
+app.use("/api/patients", patientRouter);
 
 app.get("/api/ping", (_request, response) => {
   console.log("pin pon pin pon!");
