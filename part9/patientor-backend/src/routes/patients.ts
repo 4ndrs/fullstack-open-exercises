cf6cab627981e43eb4802d156f63a4ef984a907f
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-18 09:11:01 +0100

    Part9: (patientor-backend) Add post handler to patients router

--- part9/patientor-backend/src/routes/patients.ts
+++ part9/patientor-backend/src/routes/patients.ts
@@ -1,10 +1,24 @@
 import express from "express";
 import patientService from "../services/patientService";
+import { parseRequestBody, BodyFields } from "../patientBodyParser";
 
 const router = express.Router();
 
 router.get("/", (_request, response) => {
   response.send(patientService.getNonSensitiveEntries());
+});
+
+router.post("/", (request, response) => {
+  try {
+    const newPatient = parseRequestBody(request.body as BodyFields);
+    response.status(201).send(patientService.addPatient(newPatient));
+  } catch (error: unknown) {
+    let errorMessage = "something happened";
+    if (error instanceof Error) {
+      errorMessage += ": " + error.message;
+    }
+    response.status(400).send({ error: errorMessage });
+  }
 });
 
 export default router;
