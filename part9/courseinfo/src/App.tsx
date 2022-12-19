5a0bf07f5f71b3f91197a5d8c2f9e6c5867cc5dc
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-19 02:49:14 +0100

    Part9: Add courseinfo

---
+++ part9/courseinfo/src/App.tsx
@@ -0,0 +1,59 @@
+interface CoursePart {
+  name: string;
+  exerciseCount: number;
+}
+
+const App = () => {
+  const courseName = "Half Stack application development";
+
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7,
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header name={courseName} />
+      <Content courseParts={courseParts} />
+      <Total courseParts={courseParts} />
+    </div>
+  );
+};
+
+const Header = ({ name }: { name: string }) => <h1>{name}</h1>;
+
+const Content = ({ courseParts }: { courseParts: Array<CoursePart> }) => {
+  return (
+    <>
+      {courseParts.map((part, index) => (
+        <p key={index}>
+          {part.name} {part.exerciseCount}
+        </p>
+      ))}
+    </>
+  );
+};
+
+const Total = ({ courseParts }: { courseParts: Array<CoursePart> }) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {courseParts.reduce(
+        (totalCount, part) => totalCount + part.exerciseCount,
+        0
+      )}
+    </p>
+  );
+};
+
+export default App;
