e031125d5103a432a4b491786615fbe3d8df2322
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-01-20 07:40:18 +0100

    Part9: (patientor-backend) Add entry request body parser

--- part9/patientor-backend/src/patientBodyParser.ts
+++ part9/patientor-backend/src/patientBodyParser.ts
@@ -1,4 +1,4 @@
-import { Gender, NewPatient } from "./types";
+import { Entry, Gender, HealthCheckRating, NewPatient } from "./types";
 
 export interface BodyFields {
   name: unknown;
@@ -57,4 +57,132 @@ const isDate = (value: string): boolean => {
 
 const isGender = (value: string): value is Gender => {
   return Object.values(Gender).includes(value as Gender);
+};
+
+export const parseEntryRequestBody = (requestBody: unknown): Entry => {
+  if (!isEntry(requestBody)) {
+    throw new Error("invalid entry");
+  }
+
+  const base = {
+    id: requestBody.id,
+    description: requestBody.description,
+    date: requestBody.date,
+    specialist: requestBody.specialist,
+    diagnosisCodes: requestBody?.diagnosisCodes,
+  };
+
+  let entry: Entry;
+
+  switch (requestBody.type) {
+    case "Hospital":
+      entry = {
+        ...base,
+        type: requestBody.type,
+        discharge: requestBody.discharge,
+      };
+      break;
+    case "HealthCheck":
+      entry = {
+        ...base,
+        type: requestBody.type,
+        healthCheckRating: requestBody.healthCheckRating,
+      };
+      break;
+    case "OccupationalHealthcare":
+      entry = {
+        ...base,
+        type: requestBody.type,
+        employerName: requestBody.employerName,
+        sickLeave: requestBody?.sickLeave,
+      };
+      break;
+  }
+
+  return entry;
+};
+
+const isEntry = (value: unknown): value is Entry => {
+  // check type prop
+  if (
+    !value ||
+    typeof value !== "object" ||
+    !("type" in value) ||
+    typeof value.type !== "string"
+  ) {
+    return false;
+  }
+
+  // check baseEntry props
+  if (
+    !("id" in value) ||
+    !("description" in value) ||
+    !("date" in value) ||
+    !("specialist" in value) ||
+    typeof value.id !== "string" ||
+    typeof value.description !== "string" ||
+    typeof value.date !== "string" ||
+    typeof value.specialist !== "string"
+  ) {
+    return false;
+  }
+
+  if ("diagnosisCodes" in value) {
+    if (!Array.isArray(value.diagnosisCodes)) {
+      return false;
+    }
+
+    if (value.diagnosisCodes.some((code) => typeof code !== "string"))
+      return false;
+  }
+
+  switch (value.type) {
+    case "Hospital":
+      if (
+        !("discharge" in value) ||
+        typeof value.discharge !== "object" ||
+        !value.discharge ||
+        !("date" in value.discharge) ||
+        !("criteria" in value.discharge) ||
+        typeof value.discharge.date !== "string" ||
+        typeof value.discharge.criteria !== "string"
+      ) {
+        return false;
+      }
+      break;
+    case "HealthCheck":
+      if (
+        !("healthCheckRating" in value) ||
+        typeof value.healthCheckRating !== "object" ||
+        !value.healthCheckRating ||
+        typeof value.healthCheckRating !== "number" ||
+        !Object.values(HealthCheckRating).includes(value.healthCheckRating)
+      ) {
+        return false;
+      }
+      break;
+    case "OccupationalHealthcare":
+      if (
+        !("employerName" in value) ||
+        typeof value.employerName !== "string"
+      ) {
+        return false;
+      }
+
+      if (
+        "sickLeave" in value &&
+        (typeof value.sickLeave !== "object" ||
+          !value.sickLeave ||
+          !("startDate" in value.sickLeave) ||
+          !("endDate" in value.sickLeave) ||
+          typeof value.sickLeave.startDate !== "string" ||
+          typeof value.sickLeave.endDate !== "string")
+      ) {
+        return false;
+      }
+      break;
+    default:
+      return false;
+  }
+  return true;
 };
